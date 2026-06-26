8c58ec7a7b010e5da430e924441f08f1ff7c5ad9
Yasmin <ykaroline191@gmail.com>
2026-06-25 21:42:05 -0300

    contexto dinamico

--- frufrudalara/views.py
+++ frufrudalara/views.py
@@ -7,5 +7,32 @@ def login(request):
     return render(request, "frufrudalara/tela2.html")
 
 def produtos(request):
+    
+    itens = [
+        {
+            "id": 1,
+            "imagem": 'img/image 2.png',
+        
+        },
+        {
+            "id": 2,
+            "imagem": 'img/image 3.png',
+        
+        },
+        {
+            "id": 3,
+            "imagem": 'img/image 4.png',
+            
+        },
+        {
+            "id": 4,
+            "imagem": 'img/image 5.png',
+            
+        },
+    ]
+
+    context = {
+        "itens": itens,
+    }
     return render(request, "frufrudalara/tela3.html")
 
